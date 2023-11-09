d9ef43f7ed1e188ad1dcf9613180f189af7d365f
Michael <MichaelWidler@yahoo.com>
2023-11-09 09:54:07 -0800

    Correct class name.

--- Errant/Tools/SystemTools.java
+++ Errant/Tools/SystemTools.java
@@ -1,5 +1,7 @@
 package Errant.Tools;
 
+import java.util.regex.*;
+
 public class SystemTools
 {
    public static final boolean RUNNING_FROM_JAR = isRunningFromJar();
@@ -7,7 +9,8 @@ public class SystemTools
    // check if this is running from a .jar or not
    private static boolean isRunningFromJar()
    {
-      String str = ImageTools.class.getResource(ImageTools.class.getSimpleName().toString() + ".class").toString();
+      String str = SystemTools.class.getResource(SystemTools.class.getSimpleName().toString() + ".class").toString();
+      Pattern jarPattern = Pattern.compile("^jar:");
       Matcher matcher = jarPattern.matcher(str);
       return matcher.find();
    }
