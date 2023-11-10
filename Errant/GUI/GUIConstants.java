0a94199b2d0b34abc3bdd25f94c589d7c6b1acf9
Michael <MichaelWidler@yahoo.com>
2023-11-10 10:44:40 -0800

    Fix blink speeds.

--- Errant/GUI/GUIConstants.java
+++ Errant/GUI/GUIConstants.java
@@ -2,9 +2,9 @@ package Errant.GUI;
 
 public interface GUIConstants
 {
-   public static final int SLOW_BLINK_SPEED = 250;
+   public static final int SLOW_BLINK_SPEED = 1000;
    public static final int MEDIUM_BLINK_SPEED = 500;
-   public static final int FAST_BLINK_SPEED = 1000;
+   public static final int FAST_BLINK_SPEED = 250;
    public static final int TICK_RESET_INDEX = 1000; // this should be the LCM of all blink and pulse speeds
    
    public static final int DEFAULT_ACTOR_SIZE = 24;
