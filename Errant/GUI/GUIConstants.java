5690d63fb62d487f06b94e1ae960da7fe6305631
Michael <MichaelWidler@yahoo.com>
2023-11-10 10:27:07 -0800

    Add interface for constants.

---
+++ Errant/GUI/GUIConstants.java
@@ -0,0 +1,12 @@
+package Errant.GUI;
+
+public interface GUIConstants
+{
+   public static final int SLOW_BLINK_SPEED = 250;
+   public static final int MEDIUM_BLINK_SPEED = 500;
+   public static final int FAST_BLINK_SPEED = 1000;
+   
+   public static final int DEFAULT_ACTOR_SIZE = 24;
+   public static final int DEFAULT_TERRAIN_SIZE = 24;
+   public static final int DEFAULT_ITEM_SIZE = 16;
+}
