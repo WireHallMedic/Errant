b3b18a2a977dbbf689caa273233b0d48b1dc0f57
Michael <MichaelWidler@yahoo.com>
2023-11-10 10:21:31 -0800

    Create animation manager.

---
+++ Errant/GUI/AnimationManager.java
@@ -0,0 +1,15 @@
+package Errant.GUI;
+
+public class AnimationManager implements Runnable
+{
+   private static boolean runF;
+   private static boolean terminateF;
+   public static boolean slowBlink = false;
+   public static boolean mediumBlink = false;
+   public static boolean fastBlink = false;
+
+   public void run()
+   {
+   
+   }
+}
