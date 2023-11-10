29b28689acec1122f86128141a0393fc31f6c7c9
Michael <MichaelWidler@yahoo.com>
2023-11-10 10:43:59 -0800

    Implement blinking.

--- Errant/GUI/AnimationManager.java
+++ Errant/GUI/AnimationManager.java
@@ -1,15 +1,83 @@
 package Errant.GUI;
 
-public class AnimationManager implements Runnable
+public class AnimationManager implements Runnable, GUIConstants
 {
    private static boolean runF;
    private static boolean terminateF;
+   
    public static boolean slowBlink = false;
    public static boolean mediumBlink = false;
    public static boolean fastBlink = false;
+   
+   private int tickIndex;
+   private long lastMilli;
+   private Thread thread;
+   
+   public AnimationManager()
+   {
+      start();
+   }
+   
+   public void start()
+   {
+      terminateF = false;
+      runF = true;
+      thread = new Thread(this);
+      thread.start();
+   }
 
    public void run()
    {
+      lastMilli = System.currentTimeMillis();
+      long curMilli;
+      int millisElapsed;
+      while(!terminateF)
+      {
+         curMilli = System.currentTimeMillis();
+         millisElapsed = (int)(curMilli - lastMilli);
+         // loop around if less than a millisecond has elapsed
+         if(millisElapsed == 0)
+            continue;
+         // blinks and pulses are tracked even outside of the run loop
+         incrementTicks(millisElapsed);
+         while(runF)
+         {
+         
+         }
+         lastMilli = curMilli;
+         thread.yield();
+      }
+   }
    
+   private void incrementTicks(int millisElapsed)
+   {
+      for(int i = 0; i < millisElapsed; i++)
+      {
+         tickIndex++;
+         if(tickIndex >= TICK_RESET_INDEX)
+            tickIndex = 0;
+         if(tickIndex % SLOW_BLINK_SPEED == 0)
+            slowBlink = !slowBlink;
+         if(tickIndex % MEDIUM_BLINK_SPEED == 0)
+            mediumBlink = !mediumBlink;
+         if(tickIndex % FAST_BLINK_SPEED == 0)
+            fastBlink = !fastBlink;
+      }
+   }
+   
+   public static void terminate()
+   {
+      terminateF = true;
+      runF = true;
+   }
+   
+   public static void pause()
+   {
+      runF = false;
+   }
+   
+   public static void resume()
+   {
+      runF = true;
    }
 }
--- Errant/GUI/GUIConstants.java
+++ Errant/GUI/GUIConstants.java
@@ -5,6 +5,7 @@ public interface GUIConstants
    public static final int SLOW_BLINK_SPEED = 250;
    public static final int MEDIUM_BLINK_SPEED = 500;
    public static final int FAST_BLINK_SPEED = 1000;
+   public static final int TICK_RESET_INDEX = 1000; // this should be the LCM of all blink and pulse speeds
    
    public static final int DEFAULT_ACTOR_SIZE = 24;
    public static final int DEFAULT_TERRAIN_SIZE = 24;
