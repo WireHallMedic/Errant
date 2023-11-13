652829a4d07a461e247310dab5cd818e3cfd6072
Michael <MichaelWidler@yahoo.com>
2023-11-13 10:05:56 -0800

    Create SpriteConstants.

---
+++ Errant/GUI/SpriteConstants.java
@@ -0,0 +1,14 @@
+package Errant.GUI;
+
+public interface SpriteConstants
+{
+   public static final String PAPER_DOLL_BODY_FILE = "Actors/paper_doll_bodies.png";
+   public static final String PAPER_DOLL_HEAD_FILE = "Actors/paper_doll_heads.png";
+   public static final String PAPER_DOLL_GEAR_FILE = "Actors/paper_doll_gear.png";
+   public static final String SMALL_EFFECT_FILE = "Effects/oryx_effects_24.png";
+   public static final String LARGE_EFFECT_FILE = "Effects/oryx_effects_32.png";
+   public static final String CREATURE_FILE = "Actors/oryx_creatures.png";
+   public static final String TERRAIN_FILE = "Terrain/oryx_terrain.png";
+   public static final String ITEM_FILE = "Items/oryx_items.png";
+   public static final String GOLD_FILE = "Items/coins.png";
+}
