6d75819c4e24ccdfbc84ddccdd09c9c0b10d78fd
Michael <MichaelWidler@yahoo.com>
2023-11-13 10:58:09 -0800

    More constants.

--- Errant/GUI/SpriteConstants.java
+++ Errant/GUI/SpriteConstants.java
@@ -47,4 +47,58 @@ public interface SpriteConstants
          sheetLocation[1][1] = y + 1;
       }
    }
+   
+   public enum PaperDollHeadSprite
+   {
+      SHORT_HAIR     (0, 0),
+      LONG_HAIR      (1, 0),
+      LIGHT_HELMET   (2, 0),
+      HEAVY_HELMET   (3, 0);
+      
+      public int[][] sheetLocation;
+      
+      private PaperDollHeadSprite(int x, int y)
+      {
+         sheetLocation = new int[2][2];
+         sheetLocation[0][0] = x;
+         sheetLocation[0][1] = y;
+         sheetLocation[1][0] = x;
+         sheetLocation[1][1] = y + 1;
+      }
+   }
+   
+   public enum PaperDollGearSprite
+   {
+      DAGGER_MAIN    (0, 0),
+      SWORD_MAIN     (1, 0),
+      HEAVY_SWORD    (2, 0),
+      MACE_MAIN      (3, 0),
+      HAMMER_MAIN    (4, 0),
+      HEAVY_HAMMER   (5, 0),
+      AXE_MAIN       (6, 0),
+      HEAVY_AXE      (7, 0),
+      BOW            (8, 0),
+      SPEAR          (9, 0),
+      WAND           (10, 0),
+      STAFF          (11, 0),
+      LIGHT_SHIELD   (0, 2),
+      HEAVY_SHIELD   (1, 2),
+      DAGGER_OFF     (2, 2),
+      SWORD_OFF      (3, 2),
+      MACE_OFF       (4, 2),
+      HAMMER_OFF     (5, 2),
+      AXE_OFF        (6, 2),
+      ORB            (7, 2);
+      
+      public int[][] sheetLocation;
+      
+      private PaperDollHeadSprite(int x, int y)
+      {
+         sheetLocation = new int[2][2];
+         sheetLocation[0][0] = x;
+         sheetLocation[0][1] = y;
+         sheetLocation[1][0] = x;
+         sheetLocation[1][1] = y + 1;
+      }
+   }
 }
