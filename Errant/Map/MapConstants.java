aa9aed5a9c83c456d2c1c18c815ffc2e6386ec67
Michael <MichaelWidler@yahoo.com>
2023-11-16 15:18:49 -0800

    Create map constants.

---
+++ Errant/Map/MapConstants.java
@@ -0,0 +1,13 @@
+package Errant.Map;
+
+public interface MapConstants
+{
+   public static final char WALL = '#';
+   public static final char FLOOR = '.';
+   public static final char LIQUID = '~';
+   public static final char DOOR = '|';
+   public static final char TABLE = '=';
+   public static final char CHEST = 'c';
+   public static final char STAIRS_UP = '>';
+   public static final char STAIRS_DOWN = '<';
+}
