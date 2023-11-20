d693084414dc82d34a93d85fab13cabb7affda7d
Michael <MichaelWidler@yahoo.com>
2023-11-20 05:56:39 -0800

    Add basic getters and setters.

--- Errant/GUI/MapImage.java
+++ Errant/GUI/MapImage.java
@@ -1,6 +1,86 @@
 package Errant.GUI;
 
+import Errant.Map.*;
+import Errant.Tools.*;
+import java.awt.*;
+import java.awt.image.*;
+
 public class MapImage
 {
+	private BufferedImage baseBGImage;
+	private ErrantImage[][] bgImageArray;
+	private ErrantImage[][] fgImageArray;
+	private int tileSize;
+	private int columns;
+	private int rows;
 
+
+	public BufferedImage getBaseBGImage(){return baseBGImage;}
+	public ErrantImage[][] getBGImageArray(){return bgImageArray;}
+	public ErrantImage[][] getFGImageArray(){return fgImageArray;}
+	public int getTileSize(){return tileSize;}
+	public int getColumns(){return columns;}
+	public int getRows(){return rows;}
+
+
+	public void setBaseBGImage(BufferedImage b){baseBGImage = b;}
+	public void setBGImageArray(ErrantImage[][] b){bgImageArray = b;}
+	public void setFGImageArray(ErrantImage[][] f){fgImageArray = f;}
+	public void setTileSize(int t){tileSize = t;}
+	public void setColumns(int c){columns = c;}
+	public void setRows(int r){rows = r;}
+
+
+   public MapImage(int c, int r, int ts)
+   {
+      this(null, c, r, ts);
+   }
+   
+   public MapImage(BufferedImage bgImage, int c, int r, int ts)
+   {
+      baseBGImage = bgImage;
+      columns = c;
+      rows = r;
+      tileSize = ts;
+      bgImageArray = new ErrantImage[columns][rows];
+      fgImageArray = new ErrantImage[columns][rows];
+      
+      for(int x = 0; x < columns; x++)
+      for(int y = 0; y < rows; y++)
+      {
+         bgImageArray[x][y] = null;
+         fgImageArray[x][y] = null;
+      }
+   }
+   
+   public boolean isInBounds(int x, int y)
+   {
+      return x >= 0 && x < columns &&
+             y >= 0 && y < rows;
+   }
+   
+   public void setBGImage(int x, int y, ErrantImage ei)
+   {
+      if(isInBounds(x, y))
+         bgImageArray[x][y] = ei;
+   }
+   public void setFGImage(int x, int y, ErrantImage ei)
+   {
+      if(isInBounds(x, y))
+         fgImageArray[x][y] = ei;
+   }
+   
+   public ErrantImage getBGImage(int x, int y)
+   {
+      if(isInBounds(x, y))
+         return bgImageArray[x][y];
+      return null;
+   }
+   
+   public ErrantImage getFGImage(int x, int y)
+   {
+      if(isInBounds(x, y))
+         return fgImageArray[x][y];
+      return null;
+   }
 }
---
+++ Errant/Tools/MapTester.java
@@ -0,0 +1,82 @@
+package Errant.Tools;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import Errant.GUI.*;
+import Errant.Map.*;
+import java.awt.datatransfer.*;
+import java.io.*;
+
+public class MapTester extends JPanel implements ActionListener, GUIConstants
+{
+   public static final int SPRITE_SIZE = 48;
+   public static final int FRAME_WIDTH = SPRITE_SIZE * 15;
+   public static final int FRAME_HEIGHT = SPRITE_SIZE * 15;
+   private DisplayPanel displayPanel;
+   private JPanel controlPanel;
+   private JComboBox<TerrainStyleIndex> styleDD;
+   
+   private BufferedImage map;
+   
+   
+   public MapTester()
+   {
+      super();
+      setLayout(null);
+      displayPanel = new DisplayPanel();
+      displayPanel.setBackground(Color.BLACK);
+      displayPanel.setSize(FRAME_WIDTH * 3 / 4, FRAME_HEIGHT);
+      displayPanel.setLocation(0, 0);
+      this.add(displayPanel);
+      controlPanel = new JPanel();
+      controlPanel.setLayout(new GridLayout(12, 1));
+      controlPanel.setSize(FRAME_WIDTH / 4, FRAME_HEIGHT);
+      controlPanel.setLocation(FRAME_WIDTH * 3 / 4, 0);
+      styleDD = new JComboBox<TerrainStyleIndex>(TerrainStyleIndex.values());
+      controlPanel.add(styleDD);
+      styleDD.addActionListener(this);
+      this.add(controlPanel);
+      
+      generateImage();
+   }
+   
+   public void actionPerformed(ActionEvent ae)
+   {
+      generateImage();
+      displayPanel.repaint();
+   }
+      
+   private void generateImage()
+   {
+      map = MapImageBuilder.generateMapImage(ZoneMap.getTestMap(), (TerrainStyleIndex)styleDD.getSelectedItem(), SPRITE_SIZE);
+   }
+        
+   // private class for display 
+   private class DisplayPanel extends JPanel
+   {
+      public DisplayPanel()
+      {
+         super();
+      }
+      
+      @Override
+      public void paint(Graphics g)
+      {
+         super.paint(g);
+         Graphics2D g2d = (Graphics2D)g;
+         g2d.drawImage(map, 0, 0, null);
+      }
+   }
+   
+   public static void main(String[] args)
+   {
+      JFrame frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setSize(FRAME_WIDTH + 20, FRAME_HEIGHT);
+      MapTester mapTester = new MapTester();
+      frame.add(mapTester);
+      frame.setVisible(true);
+   }
+}
